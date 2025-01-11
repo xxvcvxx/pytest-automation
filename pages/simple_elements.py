17e593b4f4445ab9ae808fb8fec3883a5672adc9
Olga <xvcvx.pl@gmail.com>
2025-01-11 15:29:44 +0100

    refactor

--- pages/simple_elements.py
+++ pages/simple_elements.py
@@ -14,12 +14,21 @@ class SimpleElementsPage:
     def hover_event(self):
         self.page.locator(self.hover_event_locator).hover()
 
-    def find_lebel_text(self):
+    def find_result_text(self):
         locator=self.page.locator(self.results_locator)
         locator.wait_for(state="visible",timeout=5000)
         return locator.text_content()
     
+    def find_label_text(self):
+        locator=self.page.locator(self.label_text_locator)
+        locator.wait_for(state="visible",timeout=5000)
+        return locator.text_content()
+    
+    
     #Assert
 
-    def test_lebel_after_click(self,actual_text,expect_text):
+    def test_result_text(self,actual_text,expect_text):
         assert actual_text == expect_text, f"Oczekiwano '{expect_text}', ale otrzymano '{actual_text}'"
+    
+    def test_label_text(selfself,actual_text,expect_text): # Assert method specific to label element for better clarity.
+        assert  actual_text == expect_text, f"Oczekiwano '{expect_text}', ale otrzymano '{actual_text}'"
--- tests/test_simple_elements.py
+++ tests/test_simple_elements.py
@@ -2,23 +2,40 @@ import pytest
 import time
 from pages.simple_elements import SimpleElementsPage
 
-def test(page,base_url_elements):
+def test_click_event(page, base_url_elements):
+    # Arrange
     simple_elements = SimpleElementsPage(page)
     page.goto(base_url_elements)
     
+    # Act
     simple_elements.click_button()
+    actual_result_text = simple_elements.find_result_text()
 
-    actual_label_text = simple_elements.find_lebel_text()
-    expect_label_text = 'You clicked the button!'
-    simple_elements.test_lebel_after_click(expect_label_text,actual_label_text)
-    
+    # Assert
+    expect_result_text = 'You clicked the button!'
+    simple_elements.test_result_text(expect_result_text, actual_result_text)
 
-def test_hover_event(page,base_url_elements):
+def test_hover_event(page, base_url_elements):
+    # Arrange
     simple_elements = SimpleElementsPage(page)
     page.goto(base_url_elements)
     
+    # Act
     simple_elements.hover_event()
+    actual_result_text = simple_elements.find_result_text()
 
-    actual_label_text = simple_elements.find_lebel_text()
-    expect_label_text = 'Mouse over event occurred!'
-    simple_elements.test_lebel_after_click(expect_label_text,actual_label_text)
+    # Assert
+    expect_result_text = 'Mouse over event occurred!'
+    simple_elements.test_result_text(expect_result_text, actual_result_text)
+
+def test_label_text(page, base_url_elements):
+    # Arrange
+    simple_elements = SimpleElementsPage(page)
+    page.goto(base_url_elements)
+
+    # Act
+    actual_result_text = simple_elements.find_label_text()
+
+    # Assert
+    expect_result_text = 'Some text for label'
+    simple_elements.test_result_text(expect_result_text, actual_result_text)
